1fdda67d07b75f9f983797194b34dbe1959060b5
Leon Coto <leoncoto@gmail.com>
2024-01-21 14:30:17 +0000

    add db stuff

---
+++ db/initialise.sql
@@ -0,0 +1,38 @@
+create table if not exists ah_organisations (
+    id serial primary key,
+    org_name varchar(256) unique not null
+);
+
+create table if not exists ah_accountstatus (
+    id serial primary key,
+    status_name varchar(256) unique not null
+);
+
+create table if not exists ah_roles (
+    id serial primary key,
+    role_name varchar(256) unique not null
+);
+
+create table if not exists ah_users (
+    id serial primary key,
+    username varchar(256) unique not null,
+    password varchar(256) not null,
+    first_name varchar(256),
+    last_name varchar(256),
+    organisation_id int references ah_organisations(id),
+    account_status_id int references ah_accountstatus(id),
+    role_id int references ah_roles(id)
+);
+
+insert into ah_organisations (org_name) values ('Auction House Solutions Inc.') on conflict do nothing;
+insert into ah_organisations (org_name) values ('Optimistic Traders') on conflict do nothing;
+insert into ah_organisations (org_name) values ('Institutional Investors') on conflict do nothing;
+insert into ah_organisations (org_name) values ('The Bank of England') on conflict do nothing;
+
+insert into ah_accountstatus (status_name) values ('active') on conflict do nothing;
+insert into ah_accountstatus (status_name) values ('blocked') on conflict do nothing;
+
+insert into ah_roles (role_name) values ('admin') on conflict do nothing;
+insert into ah_roles (role_name) values ('user') on conflict do nothing;
+
+insert into ah_users (username, password, first_name, last_name, organisation_id, account_status_id, role_id) values ('admin', 'admin', 'admin', 'admin', 1, 1, 1) on conflict do nothing;
