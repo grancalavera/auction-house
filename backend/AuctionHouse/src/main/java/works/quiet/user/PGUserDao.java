328faf08770359d0ddba70e87dd0aae6b100b2d8
Leon Coto <leoncoto@gmail.com>
2024-01-22 21:53:19 +0000

    deserialize UserModel

--- backend/AuctionHouse/src/main/java/works/quiet/user/PGUserDao.java
+++ backend/AuctionHouse/src/main/java/works/quiet/user/PGUserDao.java
@@ -2,8 +2,10 @@ package works.quiet.user;
 
 import lombok.extern.java.Log;
 import works.quiet.io.DBConnection;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -19,10 +21,13 @@ public class PGUserDao implements UserDao {
     public Optional<UserModel> findWithCredentials(String username, String password) {
         AtomicReference<UserModel> nullableUser = new AtomicReference<UserModel>(null);
 
-        String query = "SELECT username FROM ah_users WHERE username='" + username + "' AND password='" + password +"' LIMIT 1";
+        String query = "SELECT * FROM ah_users WHERE username='" + username + "' AND password='" + password + "'";
 
         connection.getConnection().ifPresent(conn->{
-            try (ResultSet resultSet = conn.createStatement().executeQuery(query)){
+            try (
+                    Statement statement = conn.createStatement();
+                    ResultSet resultSet = statement.executeQuery(query)
+            ){
                 if (resultSet.next()) {
                     UserModel user = deserialize(resultSet);
                     nullableUser.set(user);
@@ -39,8 +44,18 @@ public class PGUserDao implements UserDao {
         UserModel user = null;
 
         try {
-            UserModel.UserModelBuilder builder = UserModel.builder();
-            user = builder.build();
+            user = UserModel
+                    .builder()
+                    .id(resultSet.getInt("id"))
+                    .username(resultSet.getString("username"))
+                    .password("********")
+                    .firstName(resultSet.getString("first_name"))
+                    .lastName(resultSet.getString("last_name"))
+                    .accountStatus(AccountStatus.ofInt(resultSet.getInt("account_status_id")))
+                    .role(Role.ofInt(resultSet.getInt("role_id")))
+                    .build();
+
+            log.info(user.toString());
         } catch (Exception ex) {
             log.severe("failed to deserialize user: " + ex.toString());
         }
@@ -48,4 +63,8 @@ public class PGUserDao implements UserDao {
         return user;
     }
 
+    @Override
+    public Optional<UserModel> findByUsername(String username) {
+        return Optional.empty();
+    }
 }
--- backend/AuctionHouse/src/main/java/works/quiet/user/Role.java
+++ backend/AuctionHouse/src/main/java/works/quiet/user/Role.java
@@ -1,6 +1,20 @@
 package works.quiet.user;
 
 public enum Role {
-    ADMIN,
-    USER
+    ADMIN(1),
+    USER(2);
+    final int id;
+    Role(int id) {
+        this.id = id;
+    }
+
+    public static Role ofInt(int id) throws IllegalArgumentException {
+        Role role;
+        switch(id) {
+            case 1 -> role = Role.ADMIN;
+            case 2 -> role = Role.USER;
+            default -> throw new IllegalArgumentException(id + " is not a valid Role id.");
+        }
+        return role;
+    }
 }
