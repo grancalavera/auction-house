bfc43c1a230aad92c49438974c10b173f3a40b26
Leon Coto-Rodriguez <leon.coto-rodriguez@weareadaptive.com>
2024-02-05 19:14:27 +0000

    progress with test experiments

--- backend/AuctionHouse/src/test/java/works/quiet/cli/BoomCommandTest.java
+++ backend/AuctionHouse/src/test/java/works/quiet/cli/BoomCommandTest.java
@@ -3,14 +3,20 @@ package works.quiet.cli;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
 
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemErrNormalized;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BoomCommandTest {
 
     @Test
-    void call() {
+    void call() throws Exception {
         CommandLine program = new CommandLine(new BoomCommand());
-        int exitCode = program.execute();
-        assertEquals(1, exitCode);
+
+        var errText = tapSystemErrNormalized(() -> {
+            var exitCode = program.execute();
+            assertEquals(1, exitCode);
+        });
+
+        assertEquals("", errText);
     }
 }
